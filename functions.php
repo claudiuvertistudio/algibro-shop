e433fb07e69c75b368849afd79c07063fc79ceec
Cristian Ungureanu <cristian@themeisle.com>
2016-09-28 15:39:45 +0300

    #8 - remove import from style

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 add_action( 'wp_enqueue_scripts', 'algibro_shop_enqueue_styles' );
 function algibro_shop_enqueue_styles() {
-	wp_enqueue_style( 'algibro_shop-font', '//fonts.googleapis.com/css?family=Cabin:400,600|Open+Sans:400,300,600');
+	wp_enqueue_style( 'algibro_shop-font', algibro_shop_fonts_url(), array(), null );
 	wp_enqueue_style( 'algibro_shop-style',  get_template_directory_uri() . '/style.css', array(),'1.0.0');
 	wp_enqueue_script( 'algibro_shop-custom-js', get_stylesheet_directory_uri() . '/js/custom-script.js', array('jquery'), '1.0.0', true );
 
@@ -29,3 +29,28 @@ function algibro_shop_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'azera_shop_header_title' )->default = esc_html__('Algibro Shop','algibro-shop');
 }
 add_action( 'customize_register', 'algibro_shop_customize_register' , 99 );
+
+
+function algibro_shop_fonts_url(){
+	$fonts_url = '';
+	$font_families = array();
+
+	/* Translators: If there are characters in your language that are not
+	* supported by Source Sans Pro, translate this to 'off'. Do not translate
+	* into your own language.
+	*/
+	$source_sans_pro = _x( 'on', 'Source Sans Pro font: on or off', 'algibro-shop' );
+
+	if ( 'off' !== $source_sans_pro ) {
+
+		$font_families[] = 'Source Sans Pro:300,400,600,700';
+
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return esc_url_raw( $fonts_url );
+}
